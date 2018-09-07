c2438df996729f356d6ec677c64b5dc12f832bef
Henry Jackson <henryjackson@Henrys-MacBook-Air.local>
2018-09-07 12:29:00 -0700

    dropdown and zipcode functions

--- app/controllers/cdrops_controller.rb
+++ app/controllers/cdrops_controller.rb
@@ -3,12 +3,13 @@ class CdropsController < ApplicationController
 
   def index
      @cap_string = params[:term].slice(0,1).capitalize + params[:term].slice(1..-1)
-     if @cap_string.size > 2
+     if @cap_string.size > 1
        @cap_string = @cap_string.titleize
        @cites = Zipcode.order(:city).where("city like ?", "%#{@cap_string}%")
      else
         @cites = []
      end
-     render json: @cites.map(&:city).uniq
+     # render json: @cites.map(&:city).uniq
+     render json: @cites.map{|n| "#{n.city} |  #{n.state}"}.uniq
   end
 end
